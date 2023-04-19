5e593e8c2af8d99f0f6b7163902c768f6115feea
jackchealdang <jackchealdang@gmail.com>
2023-04-18 22:52:54 -0500

    Convert callback to async

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const {readFile} = require('fs');
+const {readFile} = require('fs').promises;
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/', async (request, response) => {
 
-    readFile('./home.html', 'utf8', (err,html)=> {
+    response.send(await readFile('./home.html', 'utf8'));
 
-        if(err){
-            response.status(500).send('sorry, out of order')
-        }
-
-        response.send(html);
-    })
 });
 
 app.listen(process.env.PORT || 3000, ()=> console.log('App available on http://localhost:3000'));
